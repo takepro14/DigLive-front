a689c09287b97812e507765a3cc52d610b4c37a3
takepro14 <takepro100pm@gmail.com>
2022-08-01 15:18:49 +0900

    add_axios.js

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM node:16.16.0-alpine
 
 ARG WORKDIR
+ARG API_URL
 
 ENV HOME=/${WORKDIR} \
     LANG=C.UTF-8 \
     TZ=Asia/Tokyo \
 	# コンテナのNuxt.jsをブラウザから参照するためにバインド
-    HOST=0.0.0.0
+    HOST=0.0.0.0 \
+    API_URL=${API_URL}
 
 RUN echo ${CONTAINER_PORT}
 
---
+++ plugins/axios.js
@@ -0,0 +1,14 @@
+export default ({ $axios }) => {
+	// リクエストのログ
+	$axios.onRequest((config) => {
+		console.log(config)
+	})
+	// レスポンスのログ
+	$axios.onResponse((config) => {
+		console.log(config)
+	})
+	// エラーのログ
+	$axios.onError((e) => {
+		console.log(e.response)
+	})
+}
