7663979f8643fb7d82fb35df46e4bcd4223e955e
takepro14 <takepro100pm@gmail.com>
2022-08-15 13:17:44 +0900

    commit

---
+++ components/BottomNavBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-card
+      class="overflow-hidden mx-auto"
+      height="200"
+    >
+      <v-bottom-navigation
+        absolute
+        hide-on-scroll
+        horizontal
+        scroll-target=scrollTarget
+      >
+        <v-spacer></v-spacer>
+        <v-btn
+          color="deep-purple accent-4"
+          text
+        >
+          <span>Recents</span>
+          <v-icon>mdi-history</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="deep-purple accent-4"
+          text
+        >
+          <span>Favorites</span>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="deep-purple accent-4"
+          text
+        >
+          <span>Nearby</span>
+          <v-icon>mdi-map-marker</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-bottom-navigation>
+      <v-responsive
+        id="hide-on-scroll-example"
+        class="overflow-y-auto"
+        max-height="600"
+      >
+        <v-responsive height="1500"></v-responsive>
+      </v-responsive>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    scrollTarget: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ components/TopNavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mb-16">
+    <v-app-bar
+      fixed
+      fab
+      top
+    >
+      <!-- <v-navigation-drawer
+        v-model="drawer"
+        app
+      >
+      </v-navigation-drawer>
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        >
+      </v-app-bar-nav-icon> -->
+      <v-toolbar-title>
+        {{ appName }}
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <nuxt-link
+        to="Signup"
+      >
+        <v-btn
+          outlined
+          text
+        >
+          新規登録
+        </v-btn>
+      </nuxt-link>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data ({ $config: { appName } }) {
+    return {
+      appName
+    }
+  }
+}
+</script>
