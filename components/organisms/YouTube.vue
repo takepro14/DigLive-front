30532c8771fb031fbcc1badfbc5c5b0ab7c77473
takepro14 <takepro100pm@gmail.com>
2022-09-05 09:05:33 +0900

    [fix] youtube_url パターン考慮によるロジック追加

--- components/organisms/YouTube.vue
+++ components/organisms/YouTube.vue
@@ -7,7 +7,9 @@
       @paused="paused"
       @ended="ended"
       fit-parent
+      :start="start"
     />
+    {{ getVideoId }}
   </div>
 </template>
 
@@ -23,14 +25,32 @@ export default {
       type: String
     }
   },
+  data () {
+    return {
+      start: ''
+    }
+  },
   computed: {
     getVideoId () {
       const url = this.youtube_url
-      const videoIdPrefix = url.indexOf('=') + 1
-      return url.substring(videoIdPrefix)
+      if (url.includes('v=')) {
+        const videoIdPrefix = url.indexOf('v=') + 2
+        if (url.includes('t=')) {
+          this.getVideoTimeParm(url)
+          return url.substr(videoIdPrefix, 11)
+        }
+        return url.substr(videoIdPrefix, 11)
+      } else {
+        return url.slice(-11)
+      }
     }
   },
   methods: {
+    getVideoTimeParm (urlStr) {
+      const videoTimeParmPrefix = urlStr.indexOf('t=') + 2
+      const videoTimeParm = urlStr.substring(videoTimeParmPrefix)
+      this.start = videoTimeParm
+    },
     playVideo () {
       this.player.playVideo()
     },
